eb3e7711389709b20e9e65c77b604cb279670b14
umasagar <73045207+umasagar123@users.noreply.github.com>
2022-03-30 10:13:55 +0530

    changes made

---
+++ javaprograms/.metadata/.plugins/org.eclipse.core.resources/.history/6c/70f655bce3af001c1531b59cde3663ad
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class prg3 {
+
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		System.out.print("Enter the no of students placed in CSE :");
+		int x=sc.nextInt();
+		System.out.print("Enter the no of students placed in ECE :");
+		int y=sc.nextInt();
+		System.out.print("Enter the no of students placed in Mech :");
+		int z=sc.nextInt();
+		if(x<0 || y<0 || z<0 )
+			System.out.println("Invalid input");
+		
+		
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- javaprograms/Application1/src/prg3.java
+++ javaprograms/Application1/src/prg3.java
@@ -9,8 +9,37 @@ public class prg3 {
 		int y=sc.nextInt();
 		System.out.print("Enter the no of students placed in Mech :");
 		int z=sc.nextInt();
-		if(x<0 || y<0 || z<0 )
+		if(x<0 || y<0 || z<0 ) {
 			System.out.println("Invalid input");
+		}
+		else if( x == y && y==z) {
+			System.out.println("None of the department has got the highest placement");
+		}
+		else if(x>y && x>z) {
+			System.out.println("Highest placement : CSE");
+		}
+		else if(y>z && y>x) {
+			System.out.println("Highest placement : ECE");
+		}
+		else if(y<z && x<z) {
+			System.out.println("Highest placement : MECH");
+		}
+		else if(x==y && x>z) {
+			System.out.println("Highest placement : CSE and ECE");
+		}
+		else if(y<x && x==z) {
+			System.out.println("Highest placement : CSE and MECH");
+		}
+		else if(z==y && z>x) {
+			System.out.println("Highest placement : ECE and MECH");
+		}
+		
+		
+		
+		
+		
+		
+		
 		
 		
 		
